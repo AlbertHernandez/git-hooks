916c85de1a6448dc05eba9b418f2182e076e254e
Albert <hpalbertt@gmail.com>
2019-02-13 19:19:13 +0100

    commit desde master

--- PRisCreated.js
+++ PRisCreated.js
@@ -1,4 +1,6 @@
 const githubAPI = require('./githubAPI');
+const { spawn } = require('child_process');
+const util = require('util');
 
 const existPullRequestInBranch = async (branchName, token, repo) => {
   // const repo = 'AlbertHernandez/todo-reactjs'
@@ -18,10 +20,28 @@ var args = process.argv.slice(2);
 const branchName = args[0];
 const tokenAuth = args[1];
 
+const undoLastMerge = () => {
+
+  const commandUndoMerge = 'git reset --hard HEAD~1'
+  
+  const exec = util.promisify(require('child_process').exec);
+
+  async function ls() {
+    const { stdout, stderr } = await exec(commandUndoMerge);
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+  }
+  ls();
+}
+
+//undoLastMerge();
+
 //akkssdfasas HOLAsdfsdsdsdsdfsdsdsdassdsdssdsdsd
 // hola mundo
 //desde rama2
-//rama2
+//rama2s3
+//akskas
 module.exports = {
   existPullRequestInBranch, 
+  undoLastMerge
 }
