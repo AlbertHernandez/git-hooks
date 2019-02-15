e0fb79d0df723b89961afa7b38bf5dc07f0be7f5
Albert <hpalbertt@gmail.com>
2019-02-15 14:27:28 +0100

    master

--- PRisCreated.js
+++ PRisCreated.js
@@ -2,12 +2,15 @@ const githubAPI = require('./githubAPI');
 const { spawn } = require('child_process');
 const util = require('util');
 
+const inicialize = async (token) => {
+  const client = githubAPI.initClient(token);
+  const urlRepo = getURLGitHub();
+  const ghrepo = githubAPI.initRepo(urlRepo);
+}
+
 const existPullRequestInBranch = async (branchName) => {
   const pullRequestArray = await githubAPI.listAllPR();
-
-  const exists = existPullRequestOfBranch(branchName, pullRequestArray);
-  // console.log('salida: ', exists);
-  return exists
+  return existPullRequestOfBranch(branchName, pullRequestArray);
 }
 
 const existPullRequestOfBranch = (branch, pullRequestArray = []) => {
@@ -15,39 +18,6 @@ const existPullRequestOfBranch = (branch, pullRequestArray = []) => {
                          .filter(prBranch => prBranch === branch)
                          .length === 1 ? true : false;
 }
-
-const inicialize = async (token) => {
-  // console.log('token: ', token);
-  
-  const client = githubAPI.initClient(token);
-  
-
-  const urlRepo = getURLGitHub();
-  
-  const ghrepo = githubAPI.initRepo(urlRepo);
-  
-
-}
-
-const undoLastMerge = () => {
-
-  const commandUndoMerge = 'git reset --hard HEAD~1'
-  
-  const exec = util.promisify(require('child_process').exec);
-
-  async function deshacerMerge() {
-    const { stdout, stderr } = await exec(commandUndoMerge);
-  }
-  deshacerMerge();
-}
-
-const findIdLastCommit = () => {
-  return require('child_process')
-  .execSync('git log --format="%H" -n 1')
-  .toString().trim()
-}
-
-
 
 const getURLGitHub = () => {
   return require('child_process')
@@ -58,37 +28,33 @@ const getURLGitHub = () => {
 const getCurrentBranch = () => {
   return require('child_process')
   .execSync('git rev-parse --abbrev-ref HEAD')
-  .toString();
-}
-
-const getBranchMerged = () => {
-  return require('child_process')
-  .execSync('git reflog -1 | cut -d" " -f 4 | sed "s/://"')
   .toString().trim();
 }
 
-const getLast10 = () => {
-  return require('child_process')
-  .execSync('git reflog -10 | cut -d" " -f 4 | sed "s/://"')
-  .toString().trim();
+const getIDCommitNotPushInBranch = (branch) => {
+  const command = 'git log origin/' + branch + '..HEAD --format="%H"'
+  
+  var res = require('child_process')
+  .execSync(command)
+  .toString().split('\n').map(function(id) {
+    return id.substr(0,7);
+  })
+  res.pop();
+  return res;
 }
 
+const getBranchesContainsCommitID = (commitID) => {
+  const command = 'git branch --contains ' + commitID
+  let res = require('child_process')
+  .execSync(command)
+  .toString().split('\n')
+  return res;
+}
 
-// const branchName = 'NewRama8';master
-// const tokenAuth = 'ae7808ada3d9133b6dfffceb4d661a0278ca1794';sd
-
-// inicialize(tokenAuth); skdskd
-// existPullRequestInBranch(branchName);ssdds
-//rama 10
-//rama 10 tu puta madre 2222 master conflicto haciendo nuevo oijijojiojojoj
-//mec skdkssd skdksdks
-//aksakdk skdkddf
 module.exports = {
   existPullRequestInBranch, 
-  undoLastMerge,
-  findIdLastCommit,
   inicialize,
   getCurrentBranch,
-  getBranchMerged,
-  getLast10
+  getIDCommitNotPushInBranch,
+  getBranchesContainsCommitID,
 }
