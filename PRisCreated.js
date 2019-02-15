98f35959e79a5cb6063c089d51f35f1af19c74f1
Albert <hpalbertt@gmail.com>
2019-02-15 13:53:10 +0100

    prueba rama 4

--- PRisCreated.js
+++ PRisCreated.js
@@ -82,7 +82,7 @@ const getLast10 = () => {
 //rama 10
 //rama 10 tu puta madre 2222 master conflicto haciendo nuevo oijijojiojojoj
 //mec skdkssd skdksdks
-//aksakdk
+//aksakdk skdkd
 module.exports = {
   existPullRequestInBranch, 
   undoLastMerge,
