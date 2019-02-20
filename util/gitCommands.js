b1671363430b01c1d2dfc9424c26c04115955984
Albert <hpalbertt@gmail.com>
2019-02-20 14:57:56 +0100

    pruebas master

---
+++ util/gitCommands.js
@@ -0,0 +1,30 @@
+const cp = require('child_process');
+
+const execute = command => cp.execSync(command).toString();
+
+const getURLGitHub = () =>
+  execute('git config --get remote.origin.url')
+    .split('git@github.com:')[1]
+    .split('.git')[0]
+    .trim();
+
+const getCurrentBranch = () =>
+  execute('git rev-parse --abbrev-ref HEAD').trim();
+
+const getIDCommitNotPushInBranch = branch => {
+  const res = execute(`git log origin/${branch}..HEAD --format="%H"`)
+    .split('\n')
+    .map(id => id.substr(0, 7));
+  res.pop();
+  return res;
+};
+
+const getBranchesContainsCommitIDWithoutClean = commitID =>
+  execute(`git branch --contains ${commitID}`).split('\n');
+
+module.exports = {
+  getURLGitHub,
+  getCurrentBranch,
+  getIDCommitNotPushInBranch,
+  getBranchesContainsCommitIDWithoutClean,
+};
---
+++ util/util.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const removeWhiteSpaces = array => array.filter(element => element !== '');
+
+const removeDuplicates = array => [...new Set(array)];
+
+const removeFirst2Caracters = array =>
+  array.map(element => element.substring(2));
+
+const removeNotAllowed = (array, noPermitidos) =>
+  array.filter(element => !(element in noPermitidos));
+
+module.exports = {
+  removeNotAllowed,
+  removeWhiteSpaces,
+  removeDuplicates,
+  removeFirst2Caracters,
+};
